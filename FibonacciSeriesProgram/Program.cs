ac5a822d8734cc3913f08633b887cd95025c4352
nani.madhu1117 <nani.madhu1117@gmail.com>
2021-08-06 12:19:51 +0530

    Add project files.

---
+++ FibonacciSeriesProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FibonacciSeriesProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int n3 = 0;
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            for (int i = 1; i <= 10; i++)
+            {
+               n3 = n1 + n2;
+               Console.WriteLine(n3);
+               n1 = n2;
+               n2 = n3;
+            }
+        }
+    }
+}
